4f5978a2d47d2fec8608a9fd36d30f261bd7d7d1
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-02-01 15:29:27 +0900

    add findAvaliableLoaction

--- Reverse_Othello.py
+++ Reverse_Othello.py
@@ -17,29 +17,72 @@ class CreateGame(QMainWindow, form_class):
         self.BLACK_CELL = 'b'
         self.WHITE_CELL = 'w'
         self.EMPTY_CELL = '0'
+        self.turn = 'b'
 
         self.BOARD_LEN = 8
         self.dx = [-1, -1, -1, 0, 0, 1, 1, 1]
         self.dy = [-1, 0, 1, -1, 1, -1, 0, 1]
         self.plate = [[self.EMPTY_CELL for x in range(self.BOARD_LEN)]
                       for y in range(self.BOARD_LEN)]
-        # self.initGame()
-
+        self.plateStatus = [[self.EMPTY_CELL for x in range(self.BOARD_LEN)]
+                            for y in range(self.BOARD_LEN)]
+        self.avableLocation = []
         self.createBoard()
+        self.initGame()
+        self.findAvaliable()
+        self.showMarker()
+
+    def findAvaliable(self):
+        self.avableLocation = []
+
+        for i in range(self.BOARD_LEN):
+            for j in range(self.BOARD_LEN):
+                if self.plateStatus[i][j] == self.BLACK_CELL:
+                    print("find Black")
+                    for k in range(8):
+                        x, y = i, j
+                        while(self.plateStatus[x+self.dx[k]][y+self.dy[k]] == self.WHITE_CELL):
+                            print(f'status : {k}')
+                            x = x + self.dx[k]
+                            y = y + self.dy[k]
+                        if x == i and y == j:
+                            continue
+                        elif self.plateStatus[x+self.dx[k]][y + self.dy[k]] == self.BLACK_CELL:
+                            continue
+                        elif self.plateStatus[x+self.dx[k]][y + self.dy[k]] == self.EMPTY_CELL:
+                            self.avableLocation.append(
+                                [x + self.dx[k], y + self.dy[k]])  # 길이 2자리 배열을 통째로
+        return
 
     def initGame(self):
+        self.plateStatus[3][3] = self.WHITE_CELL
+        self.plateStatus[4][4] = self.WHITE_CELL
+        self.plateStatus[3][4] = self.BLACK_CELL
+        self.plateStatus[4][3] = self.BLACK_CELL
+        # 정보 바탕으로 돌 그리기
 
-        self.plates[3][3] = self.WHITE_CELL
-        self.plates[4][4] = self.WHITE_CELL
-        self.plates[3][4] = self.BLACK_CELL
-        self.plates[4][3] = self.BLACK_CELL
+    def showMarker(self):
+        for i in range(self.BOARD_LEN):
+            for j in range(self.BOARD_LEN):
+                if self.plateStatus[i][j] == self.BLACK_CELL:
+                    self.plate[i][j].setIcon(QIcon("./image/black.png"))
+                    self.plate[i][j].setIconSize(QSize(50, 50))
+                elif self.plateStatus[i][j] == self.WHITE_CELL:
+                    self.plate[i][j].setIcon(QIcon("./image/white.png"))
+                    self.plate[i][j].setIconSize(QSize(50, 50))
+
+        for i in range(len(self.avableLocation)):
+            x = self.avableLocation[i][0]
+            y = self.avableLocation[i][1]
+            self.plate[x][y].setIcon(QIcon("./image/Othello icon.png"))
+            self.plate[x][y].setIconSize(QSize(20, 20))
 
     def initUI(self):
 
         self.setWindowTitle('Reverse Othello')
         self.setWindowIcon(QIcon('./image/Othello icon.png'))
         self.center()
-        self.show()
+        # self.show()
 
     def center(self):
 
@@ -52,21 +95,8 @@ class CreateGame(QMainWindow, form_class):
         for i in range(self.BOARD_LEN):
             for j in range(self.BOARD_LEN):
                 self.plate[i][j] = QPushButton("", self)
-                self.plate[i][j].move(60, 60)
+                self.plate[i][j].move(10+60*i, 10+60*j)
                 self.plate[i][j].resize(60, 60)
-
-
-# class Reversi(CreateGame):
-
-#     def __init__(self):
-#         super().__init__()
-#         self.showMarker()
-
-#     def showMarker(self):
-
-#         if self.plates[3][4] is self.BLACK_CELL:
-#             self.plateq_34.setIcon(QIcon("./image/black.png"))
-#             self.plate_34.setIconSize(QSize(50, 50))
 
 
 if __name__ == '__main__':
