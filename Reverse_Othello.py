3b43e222c0e9f19137af13b027ca9485f1fbbc85
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-02-02 15:32:17 +0900

    add update board

--- Reverse_Othello.py
+++ Reverse_Othello.py
@@ -1,4 +1,5 @@
 import sys
+import signal
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *  # QIcon
 from PyQt5.QtWidgets import *  # QApplication, QDesktopWidget, QMainWindow,
@@ -17,7 +18,9 @@ class CreateGame(QMainWindow, form_class):
         self.BLACK_CELL = 'b'
         self.WHITE_CELL = 'w'
         self.EMPTY_CELL = '0'
-        self.turn = 'b'
+        #self.turn = 'b'
+        self.MY_CELL = 'b'
+        self.OP_CELL = 'w'
 
         self.BOARD_LEN = 8
         self.dx = [-1, -1, -1, 0, 0, 1, 1, 1]
@@ -26,31 +29,45 @@ class CreateGame(QMainWindow, form_class):
                       for y in range(self.BOARD_LEN)]
         self.plateStatus = [[self.EMPTY_CELL for x in range(self.BOARD_LEN)]
                             for y in range(self.BOARD_LEN)]
-        self.avableLocation = []
+        self.avaliableLocation = []
         self.createBoard()
         self.initGame()
         self.findAvaliable()
         self.showMarker()
 
+    # def cell_count(self):
+    #     white = 0
+    #     black = 0
+
+    #     for i in range(self.BOARD_LEN):
+    #         for j in range(self.BOARD_LEN):
+    #             if self.board[i][j] == self.WHITE_CELL:
+    #                 white += 1
+    #             elif self.board[i][j] == self.BLACK_CELL:
+    #                 black += 1
+
+    #     return black, white
+
     def findAvaliable(self):
-        self.avableLocation = []
+        self.avaliableLocation = []
 
         for i in range(self.BOARD_LEN):
             for j in range(self.BOARD_LEN):
-                if self.plateStatus[i][j] == self.BLACK_CELL:
+                if self.plateStatus[i][j] == self.MY_CELL:
                     print("find Black")
                     for k in range(8):
                         x, y = i, j
-                        while(self.plateStatus[x+self.dx[k]][y+self.dy[k]] == self.WHITE_CELL):
+                        # (i, j)를 기준으로 상하좌우 대각선을 dx, dy로 점검
+                        while(self.plateStatus[x+self.dx[k]][y+self.dy[k]] == self.OP_CELL):
                             print(f'status : {k}')
                             x = x + self.dx[k]
                             y = y + self.dy[k]
                         if x == i and y == j:
                             continue
-                        elif self.plateStatus[x+self.dx[k]][y + self.dy[k]] == self.BLACK_CELL:
+                        elif self.plateStatus[x+self.dx[k]][y + self.dy[k]] == self.MY_CELL:
                             continue
                         elif self.plateStatus[x+self.dx[k]][y + self.dy[k]] == self.EMPTY_CELL:
-                            self.avableLocation.append(
+                            self.avaliableLocation.append(
                                 [x + self.dx[k], y + self.dy[k]])  # 길이 2자리 배열을 통째로
         return
 
@@ -61,6 +78,51 @@ class CreateGame(QMainWindow, form_class):
         self.plateStatus[4][3] = self.BLACK_CELL
         # 정보 바탕으로 돌 그리기
 
+    def clickedButton(self, btn):
+        print(btn)
+        print('clicked')
+        position = btn.objectName()
+        print(f'position : {position}')
+        print(type(position))
+        x = int(position[0])
+        y = int(position[1])
+
+        self.plateStatus[x][y] = self.OP_CELL
+
+        for i in range(8):
+            self.updateBoard(x, y, i)
+
+        print('call show')
+        print(self.plateStatus)
+        self.showMarker()
+
+        # self.MY_CELL, self.OP_CELL = self.OP_CELL, self.MY_CELL
+
+        # self.findAvaliable()
+        # self.showMarker()
+
+    def updateBoard(self, x, y, direction):
+        if self.plateStatus[x][y] == self.MY_CELL:
+            print('found!!!')
+            return 1
+        elif self.plateStatus[x][y] == self.EMPTY_CELL:
+            return 0
+
+        cur_x = x
+        cur_y = y
+
+        cur_x = cur_x + self.dx[direction]
+        cur_y = cur_y + self.dy[direction]
+
+        tmp = self.updateBoard(cur_x, cur_y, direction)
+
+        if tmp:
+            print(f'return 1 : {self.plateStatus[x][y]}')
+            self.plateStatus[x][y] = self.MY_CELL
+            return 1
+
+        return 0
+
     def showMarker(self):
         for i in range(self.BOARD_LEN):
             for j in range(self.BOARD_LEN):
@@ -70,12 +132,17 @@ class CreateGame(QMainWindow, form_class):
                 elif self.plateStatus[i][j] == self.WHITE_CELL:
                     self.plate[i][j].setIcon(QIcon("./image/white.png"))
                     self.plate[i][j].setIconSize(QSize(50, 50))
+                elif self.plateStatus[i][j] == self.EMPTY_CELL:
+                    self.plate[i][j].setEnabled(False)
 
-        for i in range(len(self.avableLocation)):
-            x = self.avableLocation[i][0]
-            y = self.avableLocation[i][1]
+        print(self.avaliableLocation)
+        for i in range(len(self.avaliableLocation)):
+            x = self.avaliableLocation[i][0]
+            y = self.avaliableLocation[i][1]
             self.plate[x][y].setIcon(QIcon("./image/Othello icon.png"))
             self.plate[x][y].setIconSize(QSize(20, 20))
+            # Avaliable Button Activate
+            self.plate[x][y].setEnabled(True)
 
     def initUI(self):
 
@@ -94,7 +161,11 @@ class CreateGame(QMainWindow, form_class):
     def createBoard(self):
         for i in range(self.BOARD_LEN):
             for j in range(self.BOARD_LEN):
+                #curPlate = self.plate[i][j]
                 self.plate[i][j] = QPushButton("", self)
+                self.plate[i][j].setObjectName(str(i)+str(j))
+                # self.plate[i][j].clicked.connect(
+                #     lambda: self.clickedButton(self.plate[i][j]))
                 self.plate[i][j].move(10+60*i, 10+60*j)
                 self.plate[i][j].resize(60, 60)
 
@@ -104,7 +175,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = CreateGame()  # create ReverseOthello instance
     myWindow.show()
-    # myWindow2 = Reversi()  # create Reversi instance
-    # myWindow2.show()
     # Enters the program into the event loop (running the program)
     app.exec_()
